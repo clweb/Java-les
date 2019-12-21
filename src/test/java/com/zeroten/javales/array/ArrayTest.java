7efea5772155818ee4b31770687089685fe5be72
clweb <48357976+clweb@users.noreply.github.com>
2019-12-21 23:36:23 +0800

    数组填充和排序

--- src/test/java/com/zeroten/javales/array/ArrayTest.java
+++ src/test/java/com/zeroten/javales/array/ArrayTest.java
@@ -12,6 +12,7 @@ package com.zeroten.javales.array;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class ArrayTest {
     @Test
@@ -102,5 +103,38 @@ public class ArrayTest {
         System.arraycopy(arr2,arr2.length-3,arr1,arr1.length-3,3);
         System.out.println("arr1="+Arrays.toString(arr1));
 
+    }
+    @Test
+    public void testFill(){
+//        示例：定义⼀个⼤⼩为 10 的 int 数组，并将数组全部初始化为指定的值 5，打印数组；
+//        然后将数组后 3 为赋 值为 3，再次打印数组。
+        System.out.println("-----原始填充------");
+        Integer[] arr1=new Integer[10];
+        for (int i = 0; i <10 ; i++) {
+            arr1[i]=5;
+        }
+        System.out.println("arr1="+Arrays.toString(arr1));
+        for (int i = arr1.length-3; i <arr1.length ; i++) {
+            arr1[i]=3;
+        }
+        System.out.println("arr1="+Arrays.toString(arr1));
+        System.out.println("------使用方法fill()填充-------");
+        Integer[] arr2=new Integer[10];
+        Arrays.fill(arr2,5);
+        System.out.println("arr2="+Arrays.toString(arr2));
+        Arrays.fill(arr2,arr2.length-3,arr2.length,3);
+        System.out.println("arr2="+Arrays.toString(arr2));
+    }
+    @Test
+    public void testSort(){
+//        示例：定义⼀个⼤⼩为 100 的 int 数组，随机给每⼀位赋值⼀个 0 ~ 100 之间的数值，
+//        然后对该数组进⾏排 序并打印排序结果。
+        int[] arr=new int[100];
+        for (int index = 0; index <arr.length ; index++) {
+            arr[index]=new Random().nextInt(100);
+        }
+        System.out.println("排序前："+Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println("排序后："+Arrays.toString(arr));
     }
 }
